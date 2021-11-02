9ead77e5b12beb8cb1c678f25d2eaba9010857ef
Ruan Aragão <ruanaragao2@gmail.com>
2021-11-02 10:59:47 -0300

    Change API base to https

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: 'http://private-92a969-processoseletivo1.apiary-mock.com' });
+const api = axios.create({ baseURL: 'https://private-92a969-processoseletivo1.apiary-mock.com' });
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
