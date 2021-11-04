8f26a95b2b55a93b7a977d39b197d5cbee48adb8
Ruan Aragão <ruanaragao2@gmail.com>
2021-11-03 22:51:30 -0300

    Build pwa

---
+++ src-pwa/custom-service-worker.js
@@ -0,0 +1,11 @@
+/*
+ * This file (which will be your service worker)
+ * is picked up by the build system ONLY if
+ * quasar.conf > pwa > workboxPluginMode is set to "InjectManifest"
+ */
+
+import { precacheAndRoute } from 'workbox-precaching';
+
+// Use with precache injection
+// eslint-disable-next-line
+precacheAndRoute(self.__WB_MANIFEST);
